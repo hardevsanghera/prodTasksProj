97b84a9e3cf3d528b864c3cb834fad1a26b73283
Hardev Sanghera <hardev_sanghera@yahoo.co.uk>
2025-01-03 08:21:44 +0000

    Still trying!

--- prodTasksProj/prodTasksProj/settings.py
+++ prodTasksProj/prodTasksProj/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['MY_ALLOWED_HOSTS']
 # Application definition
 
 INSTALLED_APPS = [
+    'prodTaskApp',
     'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -38,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'prodTaskApp',
 ]
 
 MIDDLEWARE = [
